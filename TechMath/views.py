c285781714f13b330112241d1cf7ddbd35d6ac29
luise <luisenriquevelezsantos@gmail.com>
2021-11-07 14:13:52 -0500

    Bootstrap

--- TechMath/views.py
+++ TechMath/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.http import request, response, HttpResponse, HttpResponseRedirect
+import json, math
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
 
 def startApp(request):
 
